4a947105c7d4834528d6f2b162d0e8c6cbe0d0e1
sstalwar <sagar.talwar88@gmail.com>
2017-03-08 14:32:13 +0530

    Solve Me First

---
+++ Algorithms/Warmup/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmup
+    {
+    class Program
+        {
+        static void Main (string[] args)
+            {
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            var solveMeFirst = new SolveMeFirst(val1, val2);
+            Console.ReadKey();
+            }
+        }
+    }
---
+++ Algorithms/Warmup/SolveMeFirst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmup
+    {
+    public class SolveMeFirst
+        {
+        public SolveMeFirst (int a, int b)
+            {
+            Console.WriteLine(a + b);
+            }
+        }
+    }
